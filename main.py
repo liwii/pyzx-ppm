ad51b522ab469160151f622c1c1ce90ccb322e0f
Koki Ryu <liukoki@gmail.com>
2021-06-09 09:46:09 +0900

    Update main

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ import pyzx as zx
 
 trates = np.linspace(0, 0.15, 6)
 
-heights = [20, 200]
+hs = [20, 200]
 
 lanes = 6
 
-for h in heights:
+for h in hs:
     heights = [[] for _ in range(4)]
     qubits = [[] for _ in range(4)]
     long_edges = [[] for _ in range(4)]
     tcounts = [[] for _ in range(4)]
+    circuit_gates = [[] for _ in range(4)]
 
     for tr in trates:
         height = np.zeros(4)
         qubit = np.zeros(4)
         long_edge = np.zeros(4)
         tcount = np.zeros(4)
+        circuit_gate = np.zeros(2)
         epochs = 4
         for _ in range(epochs):
             pattern = block_pattern(lanes, h, tr)
@@ -44,9 +46,12 @@ for h in heights:
             circuit = Circuit(lanes)
 
             add_pattern_to_circuit(pattern, circuit)
+
+            circuit_gate[0] += len(circuit.gates)
             graph = circuit.to_graph()
             zx.simplify.full_reduce(graph)
             c2 = zx.extract_circuit(graph).to_basic_gates()
+            circuit_gate[1] += len(c2.gates)
 
             ppm_result = PPM(lanes)
             for g in c2.gates:
@@ -68,12 +73,25 @@ for h in heights:
         qubit /= epochs
         long_edge /= epochs
         tcount /= epochs
+        circuit_gate /= epochs
 
         for i in range(4):
             heights[i].append(height[i])
             qubits[i].append(qubit[i])
             long_edges[i].append(long_edge[i])
             tcounts[i].append(tcount[i])
+            if i < 2:
+                circuit_gates[i].append(circuit_gate[i])
+
+    labels = ["Original Circuit", "Reduced Circuit"]
+    plt.figure()
+    plt.title("Circuit Size")
+    for i in range(2):
+        plt.plot(trates, circuit_gates[i],label=labels[i])
+    plt.ylabel("Gates")
+    plt.xlabel("T-Rate")
+    plt.legend()
+    plt.savefig(f"circuit-gate-{h}.png")
 
     labels = ["Original", "Original (Simple)", "Optimized", "Optimized (Simple)"]
 
